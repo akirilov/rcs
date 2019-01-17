da3facc0fe3def007a7f3cacaf3cecc2b29eb4c4
akirilov <atanas_kirilov@hotmail.com>
2019-01-17 19:33:17 +0000

    more hierarchical cleanup

--- subinstall/install-vim-plugins.sh
+++ subinstall/install-vim-plugins.sh
@@ -15,19 +15,19 @@ if [ ! -d ~/.vim/bundle/nerdtree ]; then
   git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
   echo "Nerdtree installed!"
 else
-  pushd ~/.vim/bundle/nerdtree
+  pushd ~/.vim/bundle/nerdtree > /dev/null
   git pull
   echo "Nerdtree updated!"
-  popd
+  popd > /dev/null
 fi
 
 # UndoTree
 if [ ! -d ~/.vim/bundle/undotree ]; then
   git clone https://github.com/mbbill/undotree.git ~/.vim/bundle/undotree
-  echo "Undotree updated!"
+  echo "Undotree installed!"
 else
-  pushd ~/.vim/bundle/undotree
+  pushd ~/.vim/bundle/undotree > /dev/null
   git pull
   echo "Undotree updated!"
-  popd
+  popd > /dev/null
 fi
