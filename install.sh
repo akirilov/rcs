72c2e8f260a9d0c664c8894ba1d6e7fea48b8af1
nas-kirilov <nas.kirilov@woven-planet.global>
2022-04-13 23:07:30 +0900

    fixing install stuff for zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Install dotfiles
-rsync -axuv ./home/ ~/
-
 # Sub-installations
 ./subinstall/install-apt-core.sh
 ./subinstall/install-apt-extra.sh
 ./subinstall/install-vim-plugins.sh
+./subinstall/install-omz.sh
+
+# Install dotfiles
+rsync -axuv ./home/ ~/
--- subinstall/install-apt-core.sh
+++ subinstall/install-apt-core.sh
@@ -1,1 +1,1 @@
-sudo apt install curl git vim i3 i3status i3lock net-tools binutils
+sudo apt install curl git vim i3 i3status i3lock net-tools binutils zsh
