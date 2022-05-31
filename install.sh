6a8ee99fa2f6d5370eefab0596eac82a034cbde3
akirilov <***REMOVED***>
2022-05-31 14:18:51 +0900

    Updated OMZ script, added version checking for apt

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Sub-installations
-./subinstall/install-apt-core.sh
-./subinstall/install-apt-extra.sh
+if [ `uname` = "Linux" ];
+then
+  ./subinstall/install-apt-core.sh
+  ./subinstall/install-apt-extra.sh
+fi
 ./subinstall/install-vim-plugins.sh
 ./subinstall/install-omz.sh
 
--- subinstall/install-omz.sh
+++ subinstall/install-omz.sh
@@ -1,4 +1,6 @@
 if [ ! -d "$HOME/.oh-my-zsh" ]
 then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  $ZSH/tools/upgrade.sh
 fi
