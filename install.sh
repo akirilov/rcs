96265f1b9c1d62cd39a8d41d90c3a6527338433d
akirilov <***REMOVED***>
2022-05-31 14:22:50 +0900

    fixed apt check to specifically look for Ubuntu

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Sub-installations
-if [ `uname` = "Linux" ];
+# Ubuntu sub-install
+if uname -a | grep "Ubuntu";
 then
   ./subinstall/install-apt-core.sh
   ./subinstall/install-apt-extra.sh
 fi
+
+# Generic sub-install
 ./subinstall/install-vim-plugins.sh
 ./subinstall/install-omz.sh
 
